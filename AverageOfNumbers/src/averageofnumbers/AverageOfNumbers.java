89308b132fc8e9e9bb43584fb9307a3902a6c539
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-02-06 18:00:38 -0500

    Write a method that gives the average of 3 numbers.

---
+++ AverageOfNumbers/src/averageofnumbers/AverageOfNumbers.java
@@ -0,0 +1,11 @@
+package averageofnumbers;
+public class AverageOfNumbers {
+		double a=2;
+	 	double b=4;
+	 	double c=6;
+public static double average(double a, double b, double c)
+   {
+System.out.println("The Average is:" + average(a, b, c)+"/3" );
+	return (a + b + c) / 3;
+}
+}
