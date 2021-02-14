2eafb29e53f3e9c53dc5305117f8e06c1c9164f7
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-02-13 21:07:31 -0500

    Declared the sum and the average variables and initialized with 0.

--- AverageOfNumbers/src/averageofnumbers/AverageOfNumbers.java
+++ AverageOfNumbers/src/averageofnumbers/AverageOfNumbers.java
@@ -1,11 +1,16 @@
 package averageofnumbers;
 public class AverageOfNumbers {
-		double a=2;
-	 	double b=4;
-	 	double c=6;
+	double a=2;
+ 	double b=4;
+ 	double c=6;
+public static double average(double a, double b, double c){
+	double sum = 0.0;
+	double avg = 0.0;
+	
+	sum = (a + b + c);
+	avg = sum/3;
 
-public static double average(double a, double b, double c) {
-		return (a + b + c) / 3;
-
+System.out.println("The average is:" + avg );
+return avg;
 }
 }
